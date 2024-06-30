a151dbd72f4b92204f6a865ee84caf9e234191e3
pamalasomchith <somchithpamala@gmail.com>
2024-06-29 22:43:37 -0700

    commit 1

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 def main():
     x = int(input('Enter a number for x-coordinate: '))
     y = int(input('Enter a number for y-coordinate: '))
-
-if (x > 0) and (y>0):
-    quadrant = 1
-
+   
+    if (int(x) > 0) and (int(y) >0):
+        quadrant = 1
+    elif (int(x) < 0) and (int(y) > 0):
+        quadrant = 2
+    elif (int(x) < 0) and (int(y) < 0):
+        quadrant = 3
+    else:
+        quadrant 4
+        
     print(f'Quadrant: {quadrant}')
     ########################################
     # Do not delete the return statement
